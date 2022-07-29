fff3b5ac7b30926f9614e151f27b685613a852ae
emersonbferreira <emerson.b.f02@outlook.com>
2022-07-29 10:10:20 -0300

    new amount_calculate

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -23,8 +23,11 @@ class EnrollmentsController < ApplicationController
     end
     today = Date.new(2018, 8, 15)
     due_date = Date.new(2018, 8, @enrollment.invoice_due_date)
-    invoice_amount = @enrollment.total_value/@enrollment.number_invoices
+    invoice_amount = amount_calculate(@enrollment.total_value, @enrollment.number_invoices)
+    amount_remainder = remainder_calculate(invoice_amount, @enrollment.number_invoices, @enrollment.total_value)
     (1..@enrollment.number_invoices).step(1) do |n|
+      if n == @enrollment.number_invoices:
+        invoice_amount += amount_remainder
       if due_date >= today.day
         @enrollment.invoice.create!(invoice_amount: invoice_amount, due_date: due_date + n.months, enrollment_id: @enrollment.id, status: 'Aberta')
       else
@@ -56,5 +59,16 @@ class EnrollmentsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def enrollment_params
       params.permit(:total_value, :number_invoices, :invoice_due_date, :institution_id, :student_id)
+    end
+    
+    #Calculate value of invoice
+    def amount_calculate(total_value,number_invoices)
+      invoice_amount = total_value.to_f / number_invoices.to_f
+      invoice_amount = invoice_amount.round(2)
+    end
+    
+    #Calculate value vestige in amount_calculate
+    def remainder_calculate(invoice_amount, number_invoices, total_value)
+      amount_remainder = ((invoice_amount * number_invoices) - total_value).round(2)
     end
 end
